a3214b8250d4fd1375034094816899fc77736925
OscarEscamilla <escamillaluquenoo@gmail.com>
2021-08-07 12:58:56 -0500

    first commit added viewbinding and navigation components

---
+++ app/src/main/java/com/example/moviesapp/MovieFragment.kt
@@ -0,0 +1,25 @@
+package com.example.moviesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moviesapp.databinding.FragmentMovieBinding
+
+
+class MovieFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentMovieBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMovieBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
